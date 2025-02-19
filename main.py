cb51b58e092a02f936b8026d1b25e9aa0df76a48
Raghav Tigadi <raghavtigadi22@gmail.com>
2025-02-18 18:34:26 -0700

    all_services_updated

---
+++ main.py
@@ -0,0 +1,8 @@
+import list_ds
+import tuple_ds
+
+my_list = list_ds.create_list()
+list_ds.add_element(my_list, 5)
+
+my_tuple = tuple_ds.create_tuple([1,2,3,4])
+tuple_ds.length(my_tuple)
---
+++ tuple_ds.py
@@ -0,0 +1,5 @@
+def create_tuple(my_tuple):  return tuple(my_tuple)
+
+def length(my_tuple):
+    print("the length of tuple is", len(my_tuple))
+    
